62a4ab55a9c60344a5ba6f514ce31f581a9a3c72
NIKA <NIKA@WIN-DU6B2AH9M6B>
2022-01-01 23:12:38 +0200

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1k2c_1._3_2_
+{
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private int _pageCount;
+        private double _price;
+
+        public Book() { }
+
+        public Book(string title, string author, int pageCount, double price)
+        {
+            Title = title;
+            Author = author;
+            PageCount = pageCount;
+            Price = price;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value != null ? value : "Нет названия книги";
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value != null ? value : "Нет автора";
+        }
+
+        public int PageCount
+        {
+            get => _pageCount;
+            set => _pageCount = value != 0 ? value : 0;
+        }
+        public double Price
+        {
+            get => _price;
+            set => _price = value != 0 ? value : 0;
+        }
+        public override string ToString() => $"{Title} {Author} {PageCount} {Price}";
+        public static Book[] GetTheMostPagesBook(Book[] books)
+        {
+            Book[] newOne = new Book[books.Length];
+            for (int i = 0; i < books.Length; i++)
+                newOne[i] = books[i];
+            foreach (var g in books)
+            {
+                if (g.PageCount > pageCount)
+                {
+                    Array.Resize(ref newOne, newOne.Length - 1);
+                    newOne[newOne.Length - 1] = g;
+                }
+            }
+            return newOne;
+        }
+        public static void GetLeafCount(string title, int pageCount)
+        {
+            Console.WriteLine($"{title} {pageCount}");
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1k2c_1._3_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] bookList = new Book[]
+            {
+                new Book("Властелин колец", "Джон Р.М. Толкин", 896, 3000),
+                new Book("Гарри Поттер и философский камень", "Дж.К. Роулинг", 223, 2100),
+                new Book("Война и мир", "Л.Н. Толстой", 1225, 2450),
+                new Book("Кулинария для начинающих", "О.О. Вилкин", 105, 145),
+                new Book("С# для чайников", "Д.И. Мюллер", 608, 1500),
+                new Book("Математика 6 класс", "Д.Ю. Воронцова", 98, 60),
+            };
+            foreach (var g in Book.GetTheMostPagesBook(bookList))
+                Console.WriteLine(g);
+            foreach (var g in Book.GetTheMostPagesBook(bookList))
+                Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
